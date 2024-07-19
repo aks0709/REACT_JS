86946d2bcdc7307452f52a74bd7e2ba79fad4f0a
aks0709 <ayushkumarjgm@gmail.com>
2024-07-20 00:14:49 +0530

    Made Custom React Element using ReactDom,injected by Babel

--- FirstViteReact/src/App.jsx
+++ FirstViteReact/src/App.jsx
@@ -1,9 +1,11 @@
 import Method from "./Fun,";
 function App() {
+  const username="AYUSHHHHHHH"
   return (  // in react project we can only add one component inside return function "<></>" it is called fragment everything inside this is treated as single component
   <>
   <Method />
-  <h1>HELLO AYUSH,FIRST REACT PROJECT WITH VITE</h1>
+  <h1>user is {username}</h1>
+  <h1>HELLO AYUSH,FIRST REACT PROJECT WITH VITE !!!</h1>
   </>
   );
 }
--- FirstViteReact/src/main.jsx
+++ FirstViteReact/src/main.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+//we can also write function here
+ 
+function myApp(){
+  return(
+    //react convert this in tree structure
+    <>   
+     <p>this function made in main.jsx</p>
+     </>
+  )
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+//this is a element in which we directly give HTML
+const anotherElement=(
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
+// const reactElement = {
+//   type : 'a',
+//   props :{  //properties
+//       href:'https://www.google.com/',
+//       target:'_blank'
+//   },
+//   children : 'CLICK ME TO VISIT GOOGLE'
+// }
+
+//since we made function customRender of our own which understand above key value pairs
+
+//but this render does understand our key values of react element
+
+const username="TITAN OF WAR"
+//this element is automatically injected by the help of babel,we dont have to write function to render or inject it
+const reactElement=React.createElement(
+  'a',
+  {href:'https://www.google.com/',target:'_blank'},//{} it is mandatory field if have  no property leave empty
+   "VISIT GOOGLE,THIS IS CUSTOM REACT ELEMENT",
+  username  //we pass evaluated variables no programming here 
+  //in this way we inject variables  
+)
+
+ReactDOM.createRoot(document.getElementById('root'))
+.render(
+  //anotherElement  //not a function
+  <App/>  
+  //App()  //we can also execte method like this but <App/> is more suitable
+    //myApp() 
+    //reactElement
 )
