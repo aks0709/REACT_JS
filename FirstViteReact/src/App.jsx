13640231372829e84b0442a44311cb479c2052d1
aks0709 <ayushkumarjgm@gmail.com>
2024-07-19 22:19:48 +0530

    Learned the React flow and structure

---
+++ FirstViteReact/src/App.jsx
@@ -0,0 +1,11 @@
+import Method from "./Fun,";
+function App() {
+  return (  // in react project we can only add one component inside return function "<></>" it is called fragment everything inside this is treated as single component
+  <>
+  <Method />
+  <h1>HELLO AYUSH,FIRST REACT PROJECT WITH VITE</h1>
+  </>
+  );
+}
+
+export default App
---
+++ FirstViteReact/src/Fun,.jsx
@@ -0,0 +1,11 @@
+//The extension of file should be jsx
+//while writing function we must make sure that first letter is capital
+function Method(){
+return(
+    <>
+    <p>method is created in Fun.jsx file which is exported and imported in APP.jsx  and APP.jsx is called on main.jsx ,which means it is rendered on web</p>
+    </>
+)
+}
+
+export default Method
