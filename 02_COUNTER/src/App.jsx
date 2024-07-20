a68619fb6f4dc3c9e18dcd2695b4b9e8f86e3d29
aks0709 <ayushkumarjgm@gmail.com>
2024-07-20 20:07:44 +0530

    Learned How to Propogate Changes to web Page using Hooks in React

---
+++ 02_COUNTER/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ //let counter=5
+
+ //hooks are used to update data in U.I at multiple places
+//useState is used to propogate changes in UI
+   let [counter,setCounter]= useState(15)  //we pass counter value
+   //must be 'let'  tabhi value change hongi,const is not used
+   //yeh counter jaha kahi pe bhi likha hoga poore web page mai udhar udhar changes honge
+ const addValue=()=>{
+  if(counter<30)
+  setCounter(counter+1)
+ }
+
+ const subValue =()=>{
+  if(counter>0)
+  setCounter(counter-1)
+ }
+  return (
+    <>
+      <h1>AYUSH AND REACT</h1>
+      <h2>COUNTER VALUE: {counter}</h2>
+      <button onClick={addValue}>ADD VALUE {counter}</button>
+      <button onClick={subValue}>REMOVE VALUE {counter}</button>
+      <p>COUNTER:- {counter}</p>
+    </>
+  )
+}
+
+export default App
