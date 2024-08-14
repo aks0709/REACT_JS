0ba4b742b6029b359911314ea4d4a230102bba4e
aks0709 <ayushkumarjgm@gmail.com>
2024-08-14 23:55:39 +0530

    TO DO CONTEXTS

--- 10_ToDo_Context_Local/src/Contexts/TodoContexts.jsx
+++ 10_ToDo_Context_Local/src/Contexts/TodoContexts.jsx
@@ -1,7 +1,22 @@
 import { createContext,useContext } from "react";
-export const TodoContext=createContext({})
+export const TodoContext=createContext({
+    todos :[
+        {
+            id :1,
+            todo : "TODO MESSAGE",
+            completed : false
+        }
+    ],
+    //functionalities  -->only name no defination
+    addTodo : (todo) =>{},
+    updatedTodo : (id,todo) =>{},
+    deleteTodo : (id) =>{},
+    toggleComplete: (id)=>{}
+})
 
-
+//method
 export const useTodo=()=>{
-    
+    return useContext(TodoContext)
 }
+
+export const TodoProvider=TodoContext.Provider 
